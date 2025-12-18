4804ad7b64fa6d231c1bc9036a397bec9cc81060
loehur <loehur@gmail.com>
2025-12-18 21:21:56 +0700

    oke

--- frontend/madinah_laundry/vite.config.js
+++ frontend/madinah_laundry/vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
+  build: {
+    outDir: "../../public/madinah_laundry",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
 })
