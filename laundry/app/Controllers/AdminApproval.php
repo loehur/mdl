848e7766fdcc58b26da3300913a96b5f4253f111
loehur <loehur@gmail.com>
2025-12-21 16:21:58 +0700

    dfa34433443353455355

--- laundry/app/Controllers/AdminApproval.php
+++ laundry/app/Controllers/AdminApproval.php
@@ -15,27 +15,27 @@ class AdminApproval extends Controller
 
       //SETORAN
       $setoran = array();
-      $where = $this->wCabang . " AND jenis_mutasi = 2 AND status_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 2 ORDER BY id_kas DESC LIMIT 20";
+      $where = $this->wCabang . " AND jenis_mutasi = 2 AND status_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 2 ORDER BY insertTime DESC LIMIT 20";
       $setoran = $this->db(0)->get_where('kas', $where);
 
       //PENGELUARAN
       $pengeluaran = array();
-      $where = $this->wCabang . " AND jenis_mutasi = 2 AND status_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 4 ORDER BY id_kas DESC LIMIT 20";
+      $where = $this->wCabang . " AND jenis_mutasi = 2 AND status_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 4 ORDER BY insertTime DESC LIMIT 20";
       $pengeluaran = $this->db(0)->get_where('kas', $where);
 
       //NON TUNAI
       $nonTunai = array();
-      $where = $this->wCabang . " AND metode_mutasi = 2 AND status_mutasi = 2 ORDER BY id_kas DESC LIMIT 20";
+      $where = $this->wCabang . " AND metode_mutasi = 2 AND status_mutasi = 2 ORDER BY insertTime DESC LIMIT 20";
       $nonTunai = $this->db(0)->get_where('kas', $where);
 
       //HAPUS ORDER
-      $where = $this->wCabang . " AND id_pelanggan <> 0 AND bin = 1 ORDER BY id_penjualan DESC LIMIT 20";
+      $where = $this->wCabang . " AND id_pelanggan <> 0 AND bin = 1 ORDER BY insertTime DESC LIMIT 20";
       $hapusOrder = $this->db(0)->get_where('sale', $where);
 
       //DEPOSIT MEMBER HAPUS
       $depositHapus = array();
       $where = $this->wCabang . " AND bin = 1";
-      $order = "id_member DESC";
+      $order = "insertTime DESC";
       $depositHapus = $this->db(0)->get_where_order('member', $where, $order);
 
       $this->view('layout', ['data_operasi' => $data_operasi]);
