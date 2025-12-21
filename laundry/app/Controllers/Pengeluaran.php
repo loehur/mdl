c0712db3613e3a0b96710cd9119b09d55add8a54
loehur <loehur@gmail.com>
2025-12-21 16:30:36 +0700

    dfa3443344335345535555

--- laundry/app/Controllers/Pengeluaran.php
+++ laundry/app/Controllers/Pengeluaran.php
@@ -12,10 +12,10 @@ class Pengeluaran extends Controller
    {
       $view = 'admin_approval/pengeluaran';
 
-      $where = $this->wCabang . " AND jenis_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 4 AND status_mutasi = 2 ORDER BY id_kas DESC LIMIT 20";
+      $where = $this->wCabang . " AND jenis_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 4 AND status_mutasi = 2 ORDER BY insertTime DESC LIMIT 20";
       $list[0] = $this->db(0)->get_where('kas', $where);
 
-      $where = $this->wCabang . " AND jenis_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 4 AND status_mutasi <> 2 ORDER BY id_kas DESC LIMIT 20";
+      $where = $this->wCabang . " AND jenis_mutasi = 2 AND metode_mutasi = 1 AND jenis_transaksi = 4 AND status_mutasi <> 2 ORDER BY insertTime DESC LIMIT 20";
       $list[1] = $this->db(0)->get_where('kas', $where);
       $this->view($view, $list);
    }
