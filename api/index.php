9bbd586e60a7d808335a3357a55f7a634834122b
loehur <loehur@gmail.com>
2025-12-22 14:27:17 +0700

    DF335555r545t5rrt

--- api/index.php
+++ api/index.php
@@ -5,4 +5,9 @@ require_once 'app/init.php';
 
 use App\Core\Route;
 
+// WORKAROUND: Fix Case Sensitivity for CMS/Chat endpoint from older Frontend
+if (isset($_GET['url']) && stripos($_GET['url'], 'cms/chat') !== false) {
+    $_GET['url'] = preg_replace('/cms\/chat/i', 'CMS/Chat', $_GET['url']);
+}
+
 $app = new Route();
