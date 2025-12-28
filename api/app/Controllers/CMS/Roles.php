9e1071955d7371de6e0a11f265c01e88a5ce91f0
loehur <loehur@gmail.com>
2025-12-28 10:10:48 +0700

    fix oke55545

--- api/app/Controllers/CMS/Roles.php
+++ api/app/Controllers/CMS/Roles.php
@@ -11,7 +11,7 @@ class Roles extends Controller
         $this->handleCors();
         
         // Fetch from Env or use Defaults (Safety fallback)
-        $data = defined('Env::USER_ROLES') ? Env::CMS_USER_ROLES : [
+        $data = defined('\Env::CMS_USER_ROLES') ? \Env::CMS_USER_ROLES : [
             'admin' => ['DEV', 'AYAH', 'IBU', 'TABLET'],
             'driver' => ['DRIVER1', 'DRIVER2'],
             'crew' => ['3', '4', '5', '6', '10', '11', '12', '13', '14']
