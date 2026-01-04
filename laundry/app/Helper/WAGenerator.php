212207929e566e1da356b9afebce18bd5ea5daad
loehur <loehur@gmail.com>
2026-01-04 21:03:35 +0700

    rapi25t6666656

--- laundry/app/Helper/WAGenerator.php
+++ laundry/app/Helper/WAGenerator.php
@@ -276,7 +276,7 @@ class WAGenerator extends Controller
             } 
         }
         
-        $output = "*" . strtoupper($nama_pelanggan) . "* _#" . $kode_cabang . "-" . $cs_code . "_\n" . $listNotif . "\n" . $totalText . "\n" . URL::HOST_URL . "/I/i/" . $id_pelanggan;
+        $output = "*" . strtoupper($nama_pelanggan) . "* _#" . $kode_cabang . "-" . $cs_code . "_\n" . $listNotif . "\n" . $totalText . "\n" . URL::HOST_URL . "/I/i/" . $id_pelanggan. "\nKetik *CEK* untuk info status laundry";
 
         // Post-processing
         $output = str_replace("<sup>2</sup>", "²", $output);
