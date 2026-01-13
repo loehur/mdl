2f107331e0d5fbdb97f56e88ed40eb9e78ad6437
loehur <loehur@gmail.com>
2026-01-13 11:09:48 +0700

    fix df5556554666665565545545

--- api/app/Config/DBC.php
+++ api/app/Config/DBC.php
@@ -8,15 +8,15 @@ class DBC
             0 => ["db" => "mdl_main", "user" => "root", "pass" => ""],
             1 => ["db" => "mdl_laundry", "user" => "root", "pass" => ""],
             2 => ["db" => "mdl_resto", "user" => "root", "pass" => ""],
-            3 => ["db" => "mdl_depot", "user" => "root", "pass" => ""],
+            3 => ["db" => "mdl_water", "user" => "root", "pass" => ""],
             4 => ["db" => "mdl_salon", "user" => "root", "pass" => ""]
         ],
         'pro' => [
-            0 => ["db" => "mdl_main", "user" => "root", "pass" => ""],
-            1 => ["db" => "mdl_laundry", "user" => "root", "pass" => ""],
-            2 => ["db" => "mdl_resto", "user" => "root", "pass" => ""],
-            3 => ["db" => "mdl_depot", "user" => "root", "pass" => ""],
-            4 => ["db" => "mdl_salon", "user" => "root", "pass" => ""]
+            0 => ["db" => "mdl_main", "user" => "mdl_main", "pass" => "wB5KjfjRYfPXBtFF"],
+            1 => ["db" => "mdl_laundry", "user" => "mdl_laundry", "pass" => "3p66WMjmPa6AmidN"],
+            2 => ["db" => "mdl_resto", "user" => "mdl_resto", "pass" => "BY4PRtSDysp8Akfz"],
+            3 => ["db" => "mdl_water", "user" => "mdl_water", "pass" => "csFW7fjxxTXB7ryR"],
+            4 => ["db" => "mdl_salon", "user" => "mdl_salon", "pass" => "W6FLRYyeKFZdTpHC"]
         ]
     ];
 }
