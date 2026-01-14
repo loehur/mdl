a1561646efa9f16260e5b2a8ab140c0cfdd5914e
loehur <loehur@gmail.com>
2026-01-14 09:41:37 +0700

    qr pay335533355655653

--- api/app/Controllers/Webhook/WhatsApp.php
+++ api/app/Controllers/Webhook/WhatsApp.php
@@ -644,7 +644,7 @@ class WhatsApp extends Controller
                 'last_in_at' => date('Y-m-d H:i:s'),
                 'last_message_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
-                'last_message' => $lastMessage,
+                'last_message' => mb_substr($lastMessage, 0, 30),
             ];
             
             // Only update case if not null (Append to existing list)
@@ -754,7 +754,7 @@ class WhatsApp extends Controller
             'last_in_at' => date('Y-m-d H:i:s'),
             'last_message_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
-            'last_message' => $lastMessage,
+            'last_message' => mb_substr($lastMessage, 0, 30),
         ];
         
         // Only set case if not null (Store as JSON List)
