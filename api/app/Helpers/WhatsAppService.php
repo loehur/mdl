19080ff9bd55a543b12676a012f717a35eb89c08
loehur <loehur@gmail.com>
2026-01-14 09:50:14 +0700

    qr pay3355333556556535

--- api/app/Helpers/WhatsAppService.php
+++ api/app/Helpers/WhatsAppService.php
@@ -706,7 +706,7 @@ class WhatsAppService
             if ($conv && $conv->num_rows() > 0) {
                 // Update conversation
                 $updateData = [
-                    'last_message' => mb_substr($lastMessageText, 0, 30),
+                    'last_message' => mb_substr($lastMessageText, 0, 40),
                     'last_message_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s')
                 ];
@@ -719,7 +719,7 @@ class WhatsAppService
                 $convData = [
                     'wa_number' => $waNumber,
                     'status' => 'closed',
-                    'last_message' => mb_substr($lastMessageText, 0, 30),
+                    'last_message' => mb_substr($lastMessageText, 0, 40),
                     'last_message_at' => date('Y-m-d H:i:s'),
                     'created_at' => date('Y-m-d H:i:s')
                 ];
