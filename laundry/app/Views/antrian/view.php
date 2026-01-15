73e863177384ef0546bc6aeb9c76747528e2d9de
loehur <loehur@gmail.com>
2026-01-15 12:25:06 +0700

    Add loading spinner to Antrian view

--- laundry/app/Views/antrian/view.php
+++ laundry/app/Views/antrian/view.php
@@ -7,13 +7,26 @@
   });
 
   $("body").dblclick(function() {
-    (".modal").hide();
+    $(".modal").hide();
   })
 
   function loadContent() {
-    $(".loaderDiv").fadeIn("fast");
-    $("div#load").load("<?= URL::BASE_URL ?>Antrian/loadList/" + <?= $data['modeView'] ?>);
-    $(".loaderDiv").fadeOut("slow");
+    // Show loading spinner
+    $("div#load").html(`
+      <div class="d-flex justify-content-center align-items-center py-5">
+        <div class="text-center">
+          <div class="spinner-border text-primary mb-3" role="status" style="width: 3rem; height: 3rem;">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+          <p class="text-muted mb-0">Memuat antrian...</p>
+        </div>
+      </div>
+    `);
+    
+    // Load content via AJAX
+    var url = "<?= URL::BASE_URL ?>Antrian/loadList/" + <?= $data['modeView'] ?>;
+    url += '?_=' + Date.now(); // Cache busting
+    $("div#load").load(url);
   }
 
   $('span.clearTuntas').click(function() {
