3eda1fcdc6067413727e41558254aa5741d37bd9
loehur <loehur@gmail.com>
2026-01-15 10:04:22 +0700

    Replace error_log with Log::write

--- laundry/app/Models/WA_YCloud.php
+++ laundry/app/Models/WA_YCloud.php
@@ -36,7 +36,7 @@ class WA_YCloud extends DB
                 // Try to extract text if possible, otherwise use raw message
                 $data['message'] = $messageData['text'] ?? $message;
                 $data['template_params'] = []; // Empty params - API will handle validation
-                error_log("[WA_YCloud] WARNING: template_params not found in JSON, using empty params. Message: " . substr($message, 0, 200));
+                Log::write("[WA_YCloud] WARNING: template_params not found in JSON, using empty params. Message: " . substr($message, 0, 200));
             }
             // ALWAYS use template mode when template_name is specified
             $data['template_name'] = $template_name;
@@ -107,9 +107,9 @@ class WA_YCloud extends DB
         
         // Log errors for debugging
         if (!$status) {
-            error_log("[WA_YCloud] FAILED | Phone: $phone | Mode: " . ($data['message_mode'] ?? 'unknown') . " | HTTP: $httpCode | Error: $msg");
+            Log::write("[WA_YCloud] FAILED | Phone: $phone | Mode: " . ($data['message_mode'] ?? 'unknown') . " | HTTP: $httpCode | Error: $msg");
             if ($decoded) {
-                error_log("[WA_YCloud] Response: " . json_encode($decoded));
+                Log::write("[WA_YCloud] Response: " . json_encode($decoded));
             }
         }
         
