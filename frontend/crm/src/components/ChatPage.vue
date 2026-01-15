a298ddc509da7131ca5196a326f446a5dff03833
loehur <loehur@gmail.com>
2026-01-15 22:55:49 +0700

    oke544444544

--- frontend/crm/src/components/ChatPage.vue
+++ frontend/crm/src/components/ChatPage.vue
@@ -641,7 +641,7 @@ onUnmounted(() => {
                   <div class="flex items-center gap-2">
                        <input type="file" ref="fileInput" @change="selectImage" accept="image/*" class="hidden" />
                        <!-- Emoji Button -->
-                       <button @click="showEmojiPicker = !showEmojiPicker" class="p-3 rounded-full bg-[var(--wa-bg-tertiary)] text-[var(--wa-icon-default)] hover:text-[var(--wa-accent-green)] hover:bg-[var(--wa-hover)] transition-all">
+                       <button @click.stop="showEmojiPicker = !showEmojiPicker" class="p-3 rounded-full bg-[var(--wa-bg-tertiary)] text-[var(--wa-icon-default)] hover:text-[var(--wa-accent-green)] hover:bg-[var(--wa-hover)] transition-all">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
