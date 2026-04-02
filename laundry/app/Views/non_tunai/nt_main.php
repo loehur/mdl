2fbe78f35466542d8c9a1f05c0ae9e8fe7c29af8
loehur <loehur@gmail.com>
2026-04-02 08:55:41 +0700

    rapi2 nontunai231

--- laundry/app/Views/non_tunai/nt_main.php
+++ laundry/app/Views/non_tunai/nt_main.php
@@ -90,11 +90,15 @@ if (count($data['cek']) == 0) { ?>
 
 <?php } ?>
 
-<!-- Modal fullscreen: tinggi = viewport; scroll hanya di dalam iframe -->
+<!-- Modal lebar, tinggi dibatasi (~80vh); scroll di dalam iframe -->
 <style>
+  #modalInvoicePelanggan .nt-modal-tagihan-dialog {
+    max-width: min(1140px, 96vw);
+    width: 96vw;
+  }
   #modalInvoicePelanggan .modal-content {
-    max-height: 100vh;
-    max-height: 100dvh;
+    max-height: 80vh;
+    max-height: 80dvh;
   }
   #modalInvoicePelanggan .nt-modal-iframe-wrap {
     flex: 1 1 auto;
@@ -109,8 +113,8 @@ if (count($data['cek']) == 0) { ?>
   }
 </style>
 <div class="modal fade" id="modalInvoicePelanggan" tabindex="-1" aria-labelledby="modalInvoicePelangganLabel" aria-hidden="true">
-  <div class="modal-dialog modal-fullscreen">
-    <div class="modal-content rounded-0 h-100 d-flex flex-column overflow-hidden">
+  <div class="modal-dialog modal-dialog-centered modal-xl nt-modal-tagihan-dialog">
+    <div class="modal-content d-flex flex-column overflow-hidden">
       <div class="modal-header flex-shrink-0 py-2 border-bottom">
         <h5 class="modal-title" id="modalInvoicePelangganLabel">Tagihan</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
