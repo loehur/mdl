316752e37fc569d7ca0310a2760af5816100937a
loehur <loehur@gmail.com>
2026-07-02 08:50:40 +0700

    fix absen

--- laundry/app/Controllers/Absen.php
+++ laundry/app/Controllers/Absen.php
@@ -31,7 +31,7 @@ class Absen extends Controller
    function absen()
    {
       $id_karyawan = (int) $_POST['karyawan'];
-      $jenis = $_POST['jenis'];
+      $jenis = (int) $_POST['jenis'];
       $tgl_post = $_POST['tgl'];
 
       $user_absen = $this->db(0)->get_where_row('user', "id_user = " . $id_karyawan . " AND en = 1");
@@ -48,17 +48,34 @@ class Absen extends Controller
          $vals = $user_absen['id_user'] . "," . $jenis . ",'" . $tgl . "','" . $jam . "'," . $_SESSION[URL::SESSID]['user']['id_cabang'];
 
 
-         if (in_array($jenis, [0, 1, 2, 3])) {
-            $where_user = "id_karyawan = " . $user_absen['id_user'] . " AND jenis = " . $jenis . " AND tanggal = '" . $tgl . "'";
-            $cek_user = $this->db(0)->count_where('absen', $where_user);
-            if ($cek_user > 0) {
-               $res = [
-                  'code' => 0,
-                  'msg' => "GAGAL - SUDAH ABSEN UNTUK JENIS INI PADA TANGGAL TERSEBUT"
-               ];
-               print_r(json_encode($res));
-               exit();
-            }
+         if (!in_array($jenis, [0, 1, 2, 3], true)) {
+            $res = [
+               'code' => 0,
+               'msg' => "GAGAL - JENIS TUGAS TIDAK VALID"
+            ];
+            print_r(json_encode($res));
+            exit();
+         }
+
+         // Grup absen per karyawan per hari (lintas cabang):
+         // - jenis 1 (Jaga Malam): maks 1x per hari
+         // - jenis 0, 2, 3 (Cuci/Delivery/Maintenance): maks 1x gabungan per hari
+         if ($jenis === 1) {
+            $where_user = "id_karyawan = " . $user_absen['id_user'] . " AND jenis = 1 AND tanggal = '" . $tgl . "'";
+            $msg_duplikat = "GAGAL - SUDAH ABSEN JAGA MALAM PADA TANGGAL TERSEBUT";
+         } else {
+            $where_user = "id_karyawan = " . $user_absen['id_user'] . " AND jenis IN (0, 2, 3) AND tanggal = '" . $tgl . "'";
+            $msg_duplikat = "GAGAL - SUDAH ABSEN CUCI/DELIVERY/MAINTENANCE PADA TANGGAL TERSEBUT";
+         }
+
+         $cek_user = $this->db(0)->count_where('absen', $where_user);
+         if ($cek_user > 0) {
+            $res = [
+               'code' => 0,
+               'msg' => $msg_duplikat
+            ];
+            print_r(json_encode($res));
+            exit();
          }
 
          //CEK MAX PER CABANG
