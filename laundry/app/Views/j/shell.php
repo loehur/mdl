08f726fcf34816242c1afaa6bf46606d401480b1
loehur <loehur@gmail.com>
2026-07-17 23:24:44 +0700

    optimasi1222212231

--- laundry/app/Views/j/shell.php
+++ laundry/app/Views/j/shell.php
@@ -27,7 +27,7 @@ $kodeCabang = $cabang['kode_cabang'] ?? '00';
   <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="<?= URL::EX_ASSETS ?>plugins/fontawesome-free-5.15.4-web/css/all.css">
   <link rel="stylesheet" href="<?= URL::EX_ASSETS ?>plugins/bootstrap-5.3/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?= $assets ?>css/j-customer.css?v=7">
+  <link rel="stylesheet" href="<?= $assets ?>css/j-customer.css?v=8">
 </head>
 <body>
 <div class="j-app"
