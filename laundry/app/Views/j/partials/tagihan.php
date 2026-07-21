502a28d99859d3b343323fccf4cca59c996511d7
loehur <loehur@gmail.com>
2026-07-21 08:47:27 +0700

    rapi2 view tagihan2112113211

--- laundry/app/Views/j/partials/tagihan.php
+++ laundry/app/Views/j/partials/tagihan.php
@@ -11,7 +11,7 @@ $customer = $data['customer'] ?? ['id' => $id, 'nama' => $p['nama_pelanggan'], '
 $hp = $customer['hp'] ?? ($p['nomor_pelanggan'] ?? '');
 $hasUnpaid = !empty($unpaid);
 $cabang = $data['cabang'] ?? [];
-$namaCabang = $cabang['nama_cabang'] ?? 'MDL Laundry';
+$namaLaundry = $cabang['nama'] ?? ($cabang['nama_cabang'] ?? 'Laundry');
 $kodeCabang = $cabang['kode_cabang'] ?? '00';
 ?>
 
@@ -282,7 +282,7 @@ $kodeCabang = $cabang['kode_cabang'] ?? '00';
     </div>
     <div class="j-preview-page" id="jPreviewPage" data-nama="<?= htmlspecialchars($namaPelanggan) ?>">
       <div class="j-preview-head">
-        <div class="j-preview-branch"><?= htmlspecialchars($namaCabang) ?> · <?= htmlspecialchars($kodeCabang) ?></div>
+        <div class="j-preview-branch"><?= htmlspecialchars($namaLaundry) ?> - <?= htmlspecialchars($kodeCabang) ?></div>
         <div class="j-preview-customer"><?= htmlspecialchars($namaPelanggan) ?></div>
       </div>
 
