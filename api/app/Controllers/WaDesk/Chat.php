46edb4417177f763723fb925c2a1e8be1841c1fc
loehur <loehur@gmail.com>
2026-09-01 16:53:29 +0700

    qris rapi233333

--- api/app/Controllers/WaDesk/Chat.php
+++ api/app/Controllers/WaDesk/Chat.php
@@ -269,9 +269,6 @@ class Chat extends WaDeskController
             if ($channelId <= 0 || $phone === '') {
                 $this->error('channel_id dan phone wajib untuk chat baru', 400);
             }
-            if (($user['role'] ?? '') === 'agent' && $mode !== 'template') {
-                $this->error('Agent hanya dapat membalas percakapan setelah mengirim template di percakapan tersebut.', 403);
-            }
             $conv = null;
         }
 
--- api/database/wadesk/migrations/052_agent_conversation_visibility_index.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE messages
-  ADD INDEX idx_msg_agent_template_visibility (conversation_id, sent_by_user_id, type, direction);
